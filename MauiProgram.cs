4b4f5588813c67a166bd7c77bd40cc3fe41fb54c
hamish taylor <hamish@raygun.com>
2023-05-03 16:39:26 +1200

    added raygun crashreporting

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Mindscape.Raygun4Maui;
 
 namespace mauitests;
 
@@ -24,6 +25,8 @@ public static class MauiProgram
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
+
+        RaygunClient.Attach("YOUR_APP_API_KEY_HERE");
 
         return builder.Build();
     }
