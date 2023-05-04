3eaed39a5ada67c4e74ed0fae5a5dc73e92dfec2
hamish taylor <hamish@raygun.com>
2023-05-04 15:57:02 +1200

    added raygun crashreporting

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,30 +1,34 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Mindscape.Raygun4Net.Maui;
 
-namespace mauitests;
-
-public static class MauiProgram
+namespace mauitests
 {
-    public static MauiApp CreateMauiApp()
+    public static class MauiProgram
     {
+        public static MauiApp CreateMauiApp()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets<MainPage>()
+                .Build();
 
-        var configuration = new ConfigurationBuilder()
-       .AddUserSecrets<MainPage>()
-       .Build();
-
-
-        var builder = MauiApp.CreateBuilder();
-        builder
+            var builder = MauiApp.CreateBuilder();
+            builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddRaygunCrashReporting(configuration.GetSection("RaygunSettings"));
                 });
 #if DEBUG
-        builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
-        return builder.Build();
+            return builder.Build();
+        }
     }
 }
