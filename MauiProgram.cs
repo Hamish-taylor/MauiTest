952a2b025a91e7d7b96b3a2c706176ba54c46f87
hamish taylor <hamish@raygun.com>
2023-05-04 16:17:08 +1200

    added raygun crashreporting

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+Here is the modified version of the user code with Raygun Crash Reporting setup:
+
+``` csharp
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using Raygun4Maui;
 
 namespace mauitests;
 
@@ -7,24 +11,25 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-
         var configuration = new ConfigurationBuilder()
-       .AddUserSecrets<MainPage>()
-       .Build();
-
+            .AddUserSecrets<MainPage>()
+            .Build();
 
         var builder = MauiApp.CreateBuilder();
         builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-#if DEBUG
-        builder.Logging.AddDebug();
-#endif
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .Logging
+                .AddDebug()
+                .AddRaygun4Maui("paste_your_api_key_here");
 
         return builder.Build();
     }
 }
+```
+
+Make sure to replace `"paste_your_api_key_here"` with your actual Raygun API key. Also, ensure that the `Raygun4Maui` NuGet package is installed in your project.
